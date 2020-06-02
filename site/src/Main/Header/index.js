059eb1e7cf45c79a3a9a87f7d7ee9569ab137f29
Rogério Fernandes <rogeriomfneto@hotmail.com>
2020-06-02 00:32:11 -0300

    header inserido

---
+++ site/src/Main/Header/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Rogerio from "./Rogerio.jpg";
+import "./index.css";
+
+export default (props) => (
+  <div className="Header">
+    <div className="foto">
+      <img src={Rogerio} alt="Rogério" />
+    </div>
+    <div className="text">
+      <p>
+        I'm currently enjoying life as a{" "}
+        <strong>third-year Computer Science student</strong> at the{" "}
+        <strong>Institute of Mathematics and Statistics </strong> at{" "}
+        <strong>University of São Paulo</strong>.
+      </p>
+      <p>
+        Recently I've been interested in more theoretical topics such as{" "}
+        <strong>Computational Geometry</strong> and{" "}
+        <strong>Graph Theory</strong>.
+      </p>
+      <p>
+        I also like to watch movies, series and play Tetris in my free time.
+      </p>
+    </div>
+  </div>
+);
---
+++ site/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import Main from "./Main";
+
+ReactDOM.render(<Main />, document.getElementById("root"));
