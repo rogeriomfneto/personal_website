a8354e8236d411d1adda31515632e5dfa9103bd7
Rogerio Fernandes <rogerio.fernandes@wildlifestudios.com>
2022-04-30 03:44:09 -0300

    new build

--- site/src/index.js
+++ site/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/~rogeriomf">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
