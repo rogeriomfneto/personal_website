af6170d6f26b84dd95c54cf37b4fc3fe41ba0f9d
Rogerio Fernandes <rogerio.fernandes@wildlifestudios.com>
2022-04-30 22:36:25 -0300

    new build

--- site/src/Routes.js
+++ site/src/Routes.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Main from "./Main";
 import Tcc from "./Tcc";
-import { Switch, Route } from "react-router";
+import { Switch, Route, Redirect } from "react-router";
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Main}/>
       <Route path="/mac0499/" component={Tcc}/>
-      <Route path="/*" component={Main}/>
+      <Redirect from="*" to="/"/>
     </Switch>
   )
 }
